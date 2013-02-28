e0f8141c96f2a03f2789f061b411a7174b4a207b
Anonoz Chong <chon_siong@hotmail.com>
2013-02-28 22:51:18 +0800

    Added table schema of mysql

--- serverside/mysql.php
+++ serverside/mysql.php
@@ -6,3 +6,8 @@ $username = '';
 $password = '';
 
 //schema below
+/*
+column - data type - what it actually is going to store
+intake - varchar(255) - string of intake code
+timetable - text - Serialized PHP array of arrays of lectures/tutorials
+*/
