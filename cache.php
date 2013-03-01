545fde77b87ec7e4dafd2bf61958cdbdd2a0accd
Anonoz Chong <chon_siong@hotmail.com>
2013-03-01 10:09:20 +0800

    Buses and cache manifest added

---
+++ cache.php
@@ -0,0 +1,20 @@
+<?php
+
+header("Contentg-type: text/cache-manifest");
+
+//the line below, don't put any whitespace
+?>CACHE MANIFEST
+#V3-16
+
+CACHE:
+logo.png
+index.html
+extras/shuttle_elc_scp.pdf
+extras/APU_Shuttle_Services_FBM_Mines_301012.pdf
+extras/APU_Shuttle_Services_TPM_301012.pdf
+
+NETWORK:
+*
+
+FALLBACK:
+online.js offline.js
